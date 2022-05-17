1f4b894bc79c83d3406e910b61bb9c4219c64490
Israel Marques Martins <email.israel.br@gmail.com>
2022-05-17 19:50:39 -0400

    Update train.py

--- train.py
+++ train.py
@@ -131,7 +131,7 @@ def get_callbacks(experiment_dir, checkpoint_monitor='val_accuracy'):
     # save model checkpoints
     filepath = os.path.join(experiment_dir, 
                             'checkpoints', 
-                            'checkpoint-epoch_{epoch:03d}-val_acc_{val_acc:.3f}.hdf5')
+                            'checkpoint-epoch_{epoch:03d}-val_accuracy_{val_accuracy:.3f}.hdf5')
 
     callbacks.append(ModelCheckpoint(filepath, 
                                      monitor=checkpoint_monitor, 
